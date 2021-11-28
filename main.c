ee836081c37093f329f6d28619bb46023b3d7bef
Bruno Cabral <cabral.brc@gmail.com>
2021-11-28 13:26:33 -0800

    struct 101

--- main.c
+++ main.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void sayHi(char name[], int age);
 double cube(double number);
 
 int main()
 {
-    //numbers();
-    //input();
-    calculator();
-    //madlibs();
-    //arrays();
-    //printf("Top\n");
-    //sayHi("John", 20);
-    //sayHi("Jane", 40);
-    //sayHi("Tom", 51);
-    //printf("Bottom\n");
-    //printf("Answer %f\n", cube(3.0));
-    //printf("Answer %f\n", cube(7.0));
+    structures();
+
     return 0;
 }
 
