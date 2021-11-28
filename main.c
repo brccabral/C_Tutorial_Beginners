6e140122b632bb15c111c0947bab92e9a7f5bd05
Bruno Cabral <cabral.brc@gmail.com>
2021-11-28 13:11:57 -0800

    funtions returns values

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void sayHi(char name[], int age);
+double cube(double number);
 
 int main()
 {
@@ -10,14 +11,20 @@ int main()
     //calculator();
     //madlibs();
     //arrays();
-    printf("Top\n");
-    sayHi("John", 20);
-    sayHi("Jane", 40);
-    sayHi("Tom", 51);
-    printf("Bottom\n");
+    //printf("Top\n");
+    //sayHi("John", 20);
+    //sayHi("Jane", 40);
+    //sayHi("Tom", 51);
+    //printf("Bottom\n");
+    printf("Answer %f\n", cube(3.0));
+    printf("Answer %f\n", cube(7.0));
     return 0;
 }
 
 void sayHi(char name[], int age){
     printf("Hello %s, you are %d\n", name, age);
+}
+
+double cube(double number){
+    return number * number * number;
 }
