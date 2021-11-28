e7d6d956268d466e1761a329079817440f3578d1
Bruno Cabral <cabral.brc@gmail.com>
2021-11-28 12:27:45 -0800

    more inputs examples

--- input.c
+++ input.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int input()
-{
+int input() {
     int age;
     printf("Enter your age: ");
     scanf("%d", &age);
@@ -17,6 +16,16 @@ int input()
     printf("Enter your grade: ");
     scanf(" %c", &grade); //need the extra space to consume the ENTER from above scanf
     printf("Your grade is %c\n", grade);
+
+    char name[20];
+    printf("Enter your name: ");
+    scanf("%s", name); // gets only until before first space
+    printf("Your name is %s\n", name);
+
+    char full_name[20];
+    printf("Enter your full name: ");
+    fgets(full_name, 20, stdin); // receives the ENTER from previous input
+    printf("Your full name is %s\n", full_name);
 
 
     return 0;
