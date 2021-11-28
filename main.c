41ebadc691b8509e1b657648f1a7934400d6ff19
Bruno Cabral <cabral.brc@gmail.com>
2021-11-28 13:06:25 -0800

    functions 101

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sayHi(char name[], int age);
+
 int main()
 {
     //numbers();
     //input();
     //calculator();
     //madlibs();
-    arrays();
+    //arrays();
+    printf("Top\n");
+    sayHi("John", 20);
+    sayHi("Jane", 40);
+    sayHi("Tom", 51);
+    printf("Bottom\n");
     return 0;
+}
+
+void sayHi(char name[], int age){
+    printf("Hello %s, you are %d\n", name, age);
 }
