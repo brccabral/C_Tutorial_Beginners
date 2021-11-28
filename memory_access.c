8909feab0a6fa950637dfec9a34355730621faf0
Bruno Cabral <cabral.brc@gmail.com>
2021-11-28 15:35:35 -0800

    memory access

--- memory_access.c
+++ memory_access.c
@@ -7,11 +7,31 @@ int memory_access()
     double gpa = 3.4;
     char grade = 'A';
 
+    // print the pointer values (memory addresses)
     printf("%p\n", &age);
     printf("%p\n", &gpa);
     printf("%p\n", &grade);
 
     printf("age: %p\ngpa: %p\ngrade: %p\n", &age, &gpa, &grade);
+
+    // put the pointer values in variables
+    int *pAge = &age;
+    double *pGpa = &gpa;
+    char *pGrade = &grade;
+
+    // print the pointer value
+    printf("%p\n", pAge);
+    printf("%p\n", pGpa);
+    printf("%p\n", pGrade);
+
+    // print the orinal value from the pointer
+    printf("%d\n", *pAge);
+    printf("%f\n", *pGpa);
+    printf("%c\n", *pGrade);
+
+    // we can stack & and * as many times we want
+    printf("%d\n", *&age);
+    printf("%p\n", &*&age);
 
     return 0;
 }
